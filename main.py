5690df0ab0319984fafd3db8890d42d149ddf2fe
talonnnnn <57449088+talonnnnn@users.noreply.github.com>
2023-05-05 10:30:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ wea, temperature = get_weather()
 data = {"city":{"value":city, "color":get_random_color()},
         "weather":{"value":wea, "color":get_random_color()},
         "temperature":{"value":temperature, "color":get_random_color()},
-        "love_days":{"value":get_count(), "color":get_random_color()},
+        "love_days":{"value":get_count() + 1, "color":get_random_color()},
         "birthday_left":{"value":get_birthday(), "color":get_random_color()},
         "words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
