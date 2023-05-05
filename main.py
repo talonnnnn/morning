40860581be7581e9c153f5bee999a82c28984329
talonnnnn <57449088+talonnnnn@users.noreply.github.com>
2023-05-05 11:22:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,11 +37,11 @@ def get_birthday():
     next = next.replace(year=next.year + 1)
   return (next - today).days
 
-def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return "一想到你，我这张脸就泛起微笑"
-  return words.json()['data']['text']
+# def get_words():
+#   words = requests.get("https://api.shadiao.pro/chp")
+#   if words.status_code != 200:
+#     return "一想到你，我这张脸就泛起微笑"
+#   return words.json()['data']['text']
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
@@ -56,6 +56,6 @@ data = {"city":{"value":city, "color":get_random_color()},
         "temperature_high":{"value":temperature, "color":get_random_color()},
         "love_days":{"value":get_count() + 1, "color":get_random_color()},
         "birthday_left":{"value":get_birthday() - 1, "color":get_random_color()},
-        "words":{"value":get_words(), "color":get_random_color()}}
+#         "words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
